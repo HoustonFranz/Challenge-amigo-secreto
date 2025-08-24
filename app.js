65aabef2a44e7ec7cd8f6b0b7dd6a72a74e53244
Houston Gaona <gaonamendozafranz@gmail.com>
2025-08-23 22:00:27 -0500

    añadiendo las primeras funciones

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listadeamigos = [];
+let listanum = [1,2,3];
+
+//botón de añadir
+function agregarAmigo(){
+    let amigoañadido = listadeamigos.push(document.getElementById('amigo').value);
+    mostraramigo('listaAmigos', `${listadeamigos}`);
+    Limpiarinput();
+}
+
+function mostraramigo(elemento, msj){
+    let elementohtml= document.getElementById(elemento);
+    elementohtml.innerHTML = msj;
+}
+console.log(listanum)
+
+function Limpiarinput(){
+    document.getElementById('amigo').value='';
+}
+
