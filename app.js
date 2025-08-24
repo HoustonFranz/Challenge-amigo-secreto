0e51d46fe6fa6fe4144324626ff8dc3f8c722bb9
Houston Gaona <gaonamendozafranz@gmail.com>
2025-08-24 01:50:45 -0500

    solo falta la validación des resultado

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listadeamigos = [];
-let listanum = [1,2,3];
 
 //botón de añadir
 function agregarAmigo(){
-    let amigoañadido = listadeamigos.push(document.getElementById('amigo').value);
+    //obtenemos el valor (nombre del amigo) del input
+    let amigoañadido = document.getElementById('amigo').value;
+    //validamos de que no esté vacío
+    amigoañadido === '' ? alert('ingrese un nombre porfavor') : listadeamigos.push(amigoañadido);
+    //mostramos
     mostraramigo('listaAmigos', `${listadeamigos}`);
-    Limpiarinput();
+    Limpiarinput('amigo');
 }
-
-function mostraramigo(elemento, msj){
+//funcion de mostrar los nombres en la lista de html
+function mostraramigo(elemento, amigos){
     let elementohtml= document.getElementById(elemento);
-    elementohtml.innerHTML = msj;
+    //limpiamos la lista cada que se añada un nuevo amigo
+    elementohtml.innerHTML = '';
+    for(let i=0; i<listadeamigos.length; i++){
+        //se crea el <li> mediante una variable li
+        let li = document.createElement('li');
+        //se le asigna el valor del array listadeamigos con el indice del valor de i
+        li.textContent = listadeamigos[i];
+        //se inserta o anexa la etiqueta li al <ul>
+        elementohtml.appendChild(li);
+    }
 }
-console.log(listanum)
 
-function Limpiarinput(){
-    document.getElementById('amigo').value='';
+function sortearAmigo(){
+    //generamos el indice para el amigo secreto elegido (el suertudo)
+    let indicesorteado = Math.floor(Math.random()*listadeamigos.length);
+    //a suertudo le asignamos el input donde va el resultado
+    let suertudo = document.getElementById('resultado');
+    // validamos de que la lista de amigos no esté vacía
+    listadeamigos === '' ? alert('ingrese los amigos porfavor') : (suertudo.innerHTML=(`el amigo secreto sorteado es: ${listadeamigos[indicesorteado]}`));   
 }
 
+function Limpiarinput(input){
+    document.getElementById(input).value='';
+}
